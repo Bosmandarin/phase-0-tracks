a6c313680d78e51e478fc0a6f79f79d0ab78f084
Tife Odumosu <boodumosu@davidson.edu>
2017-04-15 14:52:50 -0400

    Work with instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,4 @@
-
+#Author: Tife Odumosu
 #Release 1
 class Puppy
   def initialize
@@ -9,7 +9,7 @@ class Puppy
     toy
   end
   def speak(integer)
-    integer.time do
+    integer.times do
       p "Woof!"
       end
     end
@@ -17,10 +17,18 @@ class Puppy
     p "*rolls over*"
   end
   def dog_years(human_years)
-    dog_year= human_years/7
-    dog_year
+    dog_year = human_years/7
+    #p dog_year
+    puts "Your dog is #{dog_year} old!"
   end
 end
+pug = Puppy.new
+pug.fetch("stick")
+pug.speak(3)
+pug.roll_over
+pug.dog_years(28)
+puts
+
 
 #Release 2
 class Gymnast
@@ -34,23 +42,15 @@ class Gymnast
     p "jump #{num} times"
   end
 end
-pug = Puppy.new
-pug.fetch("stick")
-pug.dog_years(28)
-flips=[]
-jumps=[]
-instance=[]
-counter =0
-until counter ==50
-  gym1=Gymnast.new
-  instance.push(gym1)
-  counter +=1
+
+instance_array = []
+counter = 1
+50.times do |flex|
+  flex = Gymnast.new
+  instance_array << flex
 end
-instance.each do |index|
-  flip1 = index.flip
-  flips.push(flip1)
-  num_jumps= index.jump(rand(10))
-  jumps.push(num_jumps)
+
+instance_array.each do |instance|
+  instance.flip
+  instance.jump(rand(10))
 end
-p flips
-p jumps
