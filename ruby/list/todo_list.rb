19c12064b86545668531b0177fdf25c51ca0e877
Tife Odumosu <boodumosu@davidson.edu>
2017-04-16 11:36:10 -0400

    Run rspec tests on a todo list program

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,40 +1,38 @@
 class TodoList
 
-def initialize(list)
-  @list = ["do the dishes", "mow the lawn"]
-end
-
-def get_items
-  @list
+def initialize(groc_list)
+  @groc_list = groc_list
 end
 
 def add_item (item)
-  #@list = ["do the dishes", "mow the lawn"]
-  @list.push(item)
+  groc_list = ["do the dishes", "mow the lawn"]
+  @groc_list.push(item)
 
 end
 
 def get_items
-  @list
+  @groc_list
 end
 
 def delete_item (item)
-  #@list = ["do the dishes", "mow the lawn"]
-  @list.delete(item)
+  @groc_list = ["do the dishes", "mow the lawn"]
+  @groc_list.delete(item)
+  @groc_list
 end
 
 def get_items
-  @list
+  @groc_list
 end
 
 def get_item (i)
-  #@list = ["do the dishes", "mow the lawn"]
-  @list[i]
+  groc_list = ["do the dishes", "mow the lawn"]
+  groc_list[i]
 end
 
 end
 
-# lists = TodoList.new(["do the dishes", "mow the lawn"])
-# p lists.add_item("mop")
-# p lists.delete_item("do the dishes")
-# p lists.get_item(0)
+groc_list = ["do the dishes", "mow the lawn"]
+list = TodoList.new(groc_list)
+p list.add_item("mop")
+p list.delete_item("do the dishes")
+p list.get_item(0)
