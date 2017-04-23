c2abf5fb6cc89d9524f1c12b7dcde4fc7d4dd24e
Tife Odumosu <boodumosu@davidson.edu>
2017-04-22 20:38:50 -0400

    GPS 2.2: Grocery List

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -3,77 +3,137 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps:
-  # [fill in any steps here]
-  # set default quantity
-  # print the list to the console [can you use one of your other methods here?]
+  # Takes the input list as a string separated by spaces
+  # Breaks the string into an array of list items
+  # Iterates over the array and stores each item under the key value pair with default value of 1
+  # returns the hash
 # output: Hash
-puts "1"
-def create_list
+
+def create_list(list_of_items)
   groc_list = Hash.new
+  list_array = list_of_items.split(' ')
+  list_array.each do |item|
+    groc_list[item] = 1
+  end
   p groc_list
 end
 
-create_list
-puts
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
 # steps:
+  # Check if the list already has that item
+  # If list already has that item, include error message
+  # add item if the item does not already exist in list
+  # Print list
 # output: Hash
-puts "2"
-def add_list(list, list_item, item_quantity)
+def add_item(list, list_item, item_quantity)
+  if list.has_key?(list_item)
+    puts "Item is already in list"
+  else
   list[list_item] = item_quantity
+  end
   p list
   list
 end
-
-my_list = add_list(create_list,"Lemonade", 2)
-add_list(my_list,"Tomatoes", 3)
-add_list(my_list,"Onions", 1)
-add_list(my_list,"Ice Cream", 4)
-puts
 
 # Method to remove an item from the list
 # input: list, list_item
 # steps:
+  # Check if item is in list
+  # If item is in list
+    # delete item
+  # If it is not
+    # Return error message
+  # Return list
 # output: Hash
-puts "3"
-def remove_list(list, list_item)
-  list.delete(list_item)
+def remove_item(list, list_item)
+  if list.has_key?(list_item)
+    list.delete(list_item)
+  else
+    puts "Item is not in list"
+  end
   p list
   list
 end
-
-remove_list(my_list,"pasta")
-puts
 
 # Method to update the quantity of an item
 # input: list, list_item, item_quantity
-# steps: Change value (quantity) according to input and use the key to access that value
+# steps:
+  # Check if item already exists in list
+  # If it does, update quantity
+  # If it doesnt send error message "Item does not exist"
+  # Change value (quantity) according to input and use the key to access that value
 # output: Hash
-puts "4"
-def update_list(list, list_item, item_quantity)
-  list[list_item] = item_quantity
+def update_quantity(list, list_item, item_quantity)
+  if list.has_key?(list_item)
+    list[list_item] = item_quantity
+  else
+    puts "Item does not exist"
+  end
   p list
   list
 end
-
-update_list(my_list,"Ice Cream", 1)
-puts
-
 
 # Method to print a list and make it look pretty
 # input: list
 # steps: For each item and respective quantity in the hash, print the list
 # output: ordered list
-puts "5"
 def beautify_list(list)
   puts "Grocery List"
   puts " Item: Quantity"
   list.each do |list_item, item_quantity|
-  puts " #{list_item} : #{item_quantity}"
-
+    puts " #{list_item} : #{item_quantity}"
+  end
 end
-end
 
+
+##Driver Code
+puts
+puts "1"
+# With Interface
+puts "Please enter your list"
+list_of_items = gets.chomp
+my_list = create_list(list_of_items)
+# without interface
+# create_list("mac cheese butter rice")
+# my_list = create_list("mac cheese butter rice")
+# my_list = add_list(create_list,"Lemonade", 2)
+
+puts
+puts "2"
+# With Interface
+puts "Please enter an item"
+list_item = gets.chomp
+puts "Please enter the quantity"
+item_quantity = gets.chomp
+add_item(my_list, list_item, item_quantity)
+# Without Interface
+# add_item(my_list,"Tomatoes", 3)
+# add_item(my_list,"Onions", 1)
+# add_item(my_list,"Ice Cream", 4)
+
+puts
+puts "3"
+# With Interface
+puts "Please delete an item"
+list_item = gets.chomp
+remove_item(my_list, list_item)
+# Without Interface
+# remove_item(my_list,"pasta")
+
+puts
+puts "4"
+# With Interface
+puts "Update an items quantity"
+puts "Please enter an item"
+list_item = gets.chomp
+puts "Please enter the quantity"
+item_quantity = gets.chomp
+update_quantity(my_list, list_item, item_quantity)
+# Without Interface
+#update_quantity(my_list, "Ice Cream", 1)
+
+puts
+puts "5"
 beautify_list(my_list)
